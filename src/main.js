dcea4beb40645375dc03f7791f7e530289dc7c11
cslf-fh <consadole.sapporo.love.football@gmail.com>
2022-02-05 02:31:55 +0900

    Firebaseの追加

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
 import phaser from './phaser';
+import { analytics } from './plugins/firebase';
 
 Vue.config.productionTip = false;
 
 new Vue({
   phaser,
+  analytics,
   render: (h) => h(App),
 }).$mount('#app');
---
+++ src/plugins/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import { getFunctions } from 'firebase/functions';
+import { getAnalytics } from 'firebase/analytics';
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+const firestore = getFirestore(app);
+const storage = getStorage(app);
+const functions = getFunctions(app);
+functions.region = 'asia-northeast1';
+const analytics = getAnalytics(app);
+
+export { firestore, storage, functions, analytics };
