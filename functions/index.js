5b9621b668716c610a23487c61d56fcefead4862
cslf-fh <consadole.sapporo.love.football@gmail.com>
2022-02-05 23:15:20 +0900

    リザルト画面にシェア機能追加

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,36 @@
 const functions = require('firebase-functions');
+const config = functions.config();
+const credential = config['credential'];
+const Twit = require('twit');
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase !');
+const T = new Twit({
+  consumer_key: credential.twitter.consumer_key,
+  consumer_secret: credential.twitter.consumer_secret,
+  access_token: credential.twitter.access_token,
+  access_token_secret: credential.twitter.access_token_secret,
+  timeout_ms: 60 * 1000,
+  strictSSL: true,
 });
+
+exports.postImage = functions
+  .region('asia-northeast1')
+  .https.onCall(async (data, context) => {
+    let imageUrl = '';
+    await T.post('media/upload', { media_data: data.image }).then(
+      async (result) => {
+        const mediaIdStr = result.data.media_id_string;
+        const meta_params = {
+          media_id: mediaIdStr,
+        };
+        await T.post('media/metadata/create', meta_params).then(async () => {
+          const params = {
+            media_ids: [mediaIdStr],
+          };
+          await T.post('statuses/update', params).then(async (result) => {
+            imageUrl = await result.data.text;
+          });
+        });
+      }
+    );
+    return imageUrl;
+  });
--- src/phaser/scenes/result.js
+++ src/phaser/scenes/result.js
@@ -1,6 +1,11 @@
 import Phaser from 'phaser';
 import Preset from '../preset';
 import Player from '../splites/player';
+import { functions } from '@/plugins/firebase';
+import { httpsCallable, connectFunctionsEmulator } from 'firebase/functions';
+if (process.env.NODE_ENV === 'development') {
+  connectFunctionsEmulator(functions, 'localhost', 5001);
+}
 
 const WIDTH = Preset.config.width;
 const HEIGHT = Preset.config.height;
@@ -42,6 +47,12 @@ class Result extends Phaser.Scene {
       'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexuiplugin.min.js',
       'rexUI',
       'rexUI'
+    );
+    this.load.scenePlugin(
+      'rexspinnerplugin',
+      'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexspinnerplugin.min.js',
+      'rexSpinner',
+      'rexSpinner'
     );
   }
 
@@ -114,8 +125,44 @@ class Result extends Phaser.Scene {
     /* ボタン押下時の処理 */
     shareButton.on(
       'button.click',
-      (button) => {
-        console.log(`Click button-${button.text}`);
+      () => {
+        /* モーダルとローディングスピナの描画 */
+        const modal = this.add.rectangle(
+          WIDTH / 2,
+          HEIGHT / 2,
+          WIDTH,
+          HEIGHT,
+          0x000000,
+          0.5
+        );
+        modal.setDepth(1).setInteractive(); // インタラクティブにして他のボタンを操作不可に
+        const spinner = this.rexSpinner.add.bars({
+          x: WIDTH / 2,
+          y: HEIGHT / 2,
+          width: 64,
+          height: 64,
+          color: 0xffffff,
+        });
+        spinner.setDepth(1);
+        this.renderer.snapshot(async (image) => {
+          const img = image.src.replace(/^data:image\/png;base64,/, ''); // base64の整形
+          let imageUrl = ''; // シェア画像のURL
+          const postImage = httpsCallable(functions, 'postImage'); // 画像を投稿する関数
+          /* 画像を投稿 */
+          await postImage({ image: img })
+            .then(async (res) => {
+              imageUrl = await res.data; // 投稿した画像のURLを取得
+            })
+            .catch(() => {}); // 投稿に失敗しても処理続行
+          const baseUrl = 'https://twitter.com/intent/tweet?'; // ツイッターのシェアURL
+          const text = `スコアは${this.SCORE}点でした。`; // シェアツイートのテキスト
+          const pageUrl = 'https://score-300.web.app/';
+          const shareUrl = `${baseUrl}text=${text}&url=${imageUrl}%20${pageUrl}`; // 最終的なシェアURL
+          window.open(shareUrl); // ツイート画面を開く
+          /* モーダルとスピナを削除 */
+          modal.destroy();
+          spinner.destroy();
+        });
       },
       this
     );
